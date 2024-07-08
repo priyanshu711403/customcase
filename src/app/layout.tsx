a5c76586482265cd296490d7157227b8e206bcac
Priyanshu Gupta <priyanshu711403@gmail.com>
2024-07-08 19:30:23 +0530

    upload page done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/toaster";
+import Steps from "@/components/Steps";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +22,12 @@ export default function RootLayout({
       <html lang="en">
          <body className={inter.className}>
             <Navbar />
-            {children}
-            <Footer />
+            <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+               <div className="flex-1 flex flex-col h-full">{children}</div>
+
+               <Footer />
+            </main>
+            <Toaster />
          </body>
       </html>
    );
